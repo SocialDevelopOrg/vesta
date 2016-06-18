bfcc35d7c418e645dec93ede0b6a369eb4613e48
darshanime <deathbullet@gmail.com>
2016-06-18 16:00:40 +0530

    add app community

--- vesta/urls.py
+++ vesta/urls.py
@@ -1,12 +1,7 @@
-from django.conf.urls import patterns, include, url
-
+from django.conf.urls import url, include
 from django.contrib import admin
-admin.autodiscover()
+from django.conf import settings
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'vesta.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+    url(r'^$', include('community.urls')),
+]
